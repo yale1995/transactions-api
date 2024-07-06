b85184d02636d6240907a2347098f2bfe20492ef
Yale Araujo <yale850@gmail.com>
2024-07-06 17:32:22 -0300

    feat: migrations and querie

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import fastify from 'fastify'
+import crypto from 'crypto'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello world!!'
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 app.listen({ port: 3333 }).then(() => {
