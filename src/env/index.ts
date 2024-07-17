32ab7a5bef8a235c876d81e2e8f9322bec2dfc02
Yale Araujo <yale850@gmail.com>
2024-07-17 14:51:50 -0300

    feat: implementing runtime type checking at env variables

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,18 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
 })
 
-export const env = envSchema.parse(process.env)
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+  console.error(
+    'Invalid environment variables: ',
+    parsedEnv.error.flatten().fieldErrors,
+  )
+  throw new Error('Invalid environment variables!')
+}
+
+export const env = parsedEnv.data
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import crypto from 'crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -16,6 +17,6 @@ app.get('/hello', async () => {
   return transaction
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('Server is runing!!')
 })
