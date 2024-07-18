fc10a67b77518746cc7bc25cb39e195d97927aba
Yale Araujo <yale850@gmail.com>
2024-07-18 18:25:51 -0300

    chore: added env variable and setted node version

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
-    host: '0.0.0.0',
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('Server is runing!!')
